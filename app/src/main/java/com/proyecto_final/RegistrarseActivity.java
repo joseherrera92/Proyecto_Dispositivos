ce05faf0bab31e9bd76eee56723bb7f9a319ef53
jose.herrera <jose.herrera@iudigital.edu.co>
2023-11-08 19:32:54 -0500

    Primer verificacion

---
+++ app/src/main/java/com/proyecto_final/RegistrarseActivity.java
@@ -0,0 +1,16 @@
+package com.proyecto_final;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class RegistrarseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registrarse);
+    }
+}
